4ea5e857ce9ea559d063b0133851afae6326ae2e
zougataga <107312482+zougataga@users.noreply.github.com>
2022-11-13 09:37:14 +0100

    Delete assets directory

--- assets/function/auto.js
+++
@@ -1,7 +0,0 @@
-const axios = require('axios');
-const search = require('./search.js');
-const auto = async (ip) => {
-    const info = (await axios.get(`https://ipapi.co/${ip}/json/`)).data;
-    return await search(info.city);
-};
-module.exports = auto;
